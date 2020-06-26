cee90687ad178bff2cbc181b7c08263d1f650f95
Preston Lee <pfhgetty@google.com>
2020-06-26 12:34:33 -0700

    Command line interface for run_tests as well as error-checking for bad directory/module names.

--- run_tests.py
+++ run_tests.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Searches for and runs all tests in this repository."""
+from absl import app
+from absl import flags
 from absl import logging
 from absl.testing import absltest
 
@@ -20,22 +22,32 @@ import subprocess
 import os
 import inspect
 from importlib import import_module
+from functools import reduce
 
-# Folders which can have tests
-TEST_FOLDERS = ['./tests', './src', './evaluations']
+FLAGS = flags.FLAGS
+
+flags.DEFINE_list(
+    'folders', ['./'], 'Comma-separated list of folders to run all tests in. \
+        If no input is given, runs all tests in this directory.')
+# Make sure all folders specified exist.
+flags.register_validator(
+    'folders',
+    lambda folder: reduce(lambda x, y: x and y, map(os.path.exists, folder)),
+    message="Specified folder(s) does not exist.")
 
 
-def find_and_add_tests():
-    """Finds all tests in TEST_FOLDERS directories and adds them to the global
-    namespace for this module.
+def find_modules(folders):
+    """Returns a list of all modules in test_folders directories.
 
-    Side Effects:
-        Adds tests to global namespace so that absltest.main() can find
-        and run them.
+    Args:
+      folders: list of folders to check for modules
+    
+    Returns:
+      A list of tuples (module_name, imported module)
     """
     # Get all python files
     list_of_files = []
-    for folder in TEST_FOLDERS:
+    for folder in folders:
         # Check subdirectories and first level directory for python files
         list_of_files.extend(glob.glob(folder + '/**/*.py', recursive=True))
 
@@ -48,34 +60,71 @@ def find_and_add_tests():
         lambda x: '.'.join(os.path.normpath(x[:-3]).split(os.path.sep)),
         test_files)
 
-    logging.info(f'Tests found: ')
+    modules = []
+    # Import modules and filter out ones that cannot be imported.
+    for module_name in test_modules:
+        try:
+            module = import_module(module_name)
+        except:
+            continue
+        modules.append((module_name, module))
 
+    return modules
+
+
+def add_tests(modules):
+    """From a list of modules, adds all TestCases to the global namespace
+    such that absltest.main() finds and runs all TestCases from modules.
+
+    Args:
+      modules: list of (module_name, imported module)
+    
+    Returns:
+      None
+    
+    Side Effects:
+        Adds tests to global namespace so that absltest.main() can find
+        and run them.
+    """
+    logging.info(f'Tests added: ')
     # Loop through all modules, import that module,
     # then link all test cases from that module to the global namespace
-    for module_name in test_modules:
+    for module_name, module in modules:
         # It's a bad idea to place test cases in __init__.py files.
         # This script does not encourage nor fulfill this behavior.
         # We do allow __init__ to be used as a package name, but this
-        # is an extreme corner case.
+        # is an extreme edge case.
         if module_name.endswith('__init__'):
             continue
 
-        logging.info(f'\tIn module {module_name}:')
-
-        # Import the module by its module name
-        module = import_module(module_name)
+        logging.info(f'  In module {module_name}:')
 
         # Loop over all members of the imported module
         for name, value in inspect.getmembers(module):
             # If the member is both a class and is a subclass of absltest.TestCase,
             # we will link it to the global namespace.
             if inspect.isclass(value) and issubclass(value, absltest.TestCase):
-                logging.info(f'\t\t{name}')
+                logging.info(f'\t{name}')
                 # Modifying globals here allows absltest.main() to find the TestCase
                 globals()[f'TEST_CASE_{module_name}_{name}'] = value
 
 
-if __name__ == '__main__':
+def find_and_add_tests(folders):
+    """Finds all modules in list of folders and adds all TestCases
+    to the global namespace.
+    
+    Args:
+      folders: list of folders to check for modules with TestCases
+    """
+    modules = find_modules(folders)
+    add_tests(modules)
+
+
+def main(argv):
     logging.set_verbosity(logging.INFO)
-    find_and_add_tests()
+    find_and_add_tests(FLAGS.folders)
     absltest.main()
+
+
+if __name__ == '__main__':
+    app.run(main)
