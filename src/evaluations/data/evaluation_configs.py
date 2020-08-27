c3f869e5bbac792f364186f61a7769de81d06b61
huangxichen1 <huangxichen@google.com>
2020-08-27 20:04:37 +0000

    Revert "Revert the reach estimator and use voc instead."
    This reverts commit 332239da4f4e6832fb4dc7302db72f4328980c6f.
    The commit is intended to be included in PR #56.

--- src/evaluations/data/evaluation_configs.py
+++ src/evaluations/data/evaluation_configs.py
@@ -584,7 +584,8 @@ def _independent_set_estimator(sketch_epsilon=None, estimate_epsilon=None):
     A SketchEstimatorConfig for the independent estimator.
   """
   if sketch_epsilon:
-    sketch_noiser = vector_of_counts.LaplaceNoiser(epsilon=sketch_epsilon)
+    sketch_noiser = independent_set_estimator.ReachNoiser(
+        epsilon=sketch_epsilon)
   else:
     sketch_noiser = None
 
@@ -596,15 +597,14 @@ def _independent_set_estimator(sketch_epsilon=None, estimate_epsilon=None):
 
   return SketchEstimatorConfig(
       name=construct_sketch_estimator_config_name(
-          sketch_name='reach_using_voc',
-          sketch_config='1',
+          sketch_name='reach',
+          sketch_config='None',
           estimator_name=f'independent_estimator_universe{UNIVERSE_SIZE_VALUE}',
           sketch_epsilon=sketch_epsilon,
           estimate_epsilon=estimate_epsilon),
-      sketch_factory=vector_of_counts.VectorOfCounts.get_sketch_factory(
-          num_buckets=1),
+      sketch_factory=independent_set_estimator.Reach.get_sketch_factory(),
       estimator=independent_set_estimator.IndependentSetEstimator(
-          vector_of_counts.SequentialEstimator(), UNIVERSE_SIZE_VALUE),
+          independent_set_estimator.ReachEstimator(), UNIVERSE_SIZE_VALUE),
       sketch_noiser=sketch_noiser,
       estimate_noiser=estimate_noiser
   )
