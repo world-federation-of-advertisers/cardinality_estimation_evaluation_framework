291214f26cb1ebd548b3af9557d4794b65c89600
Evgeny Skvortsov <evgenys@google.com>
2020-10-14 12:58:47 -0700

    Fixing warning in Liquid Legions legions_expectation.

--- src/estimators/liquid_legions.py
+++ src/estimators/liquid_legions.py
@@ -109,8 +109,9 @@ class LiquidLegions(base.SketchBase):
         a = self.a
         z = 1 - (-special.expi(- a * t / (numpy.exp(a) - 1))
                 +special.expi(-a * numpy.exp(a) * t / (numpy.exp(a) - 1))) / a
-        self.memoized_cardinality[cache_key] = z
-    return self.memoized_cardinality[cache_key]
+        r = numpy.where(t == 0, 0, z)
+        self.memoized_cardinality[cache_key] = r
+      return self.memoized_cardinality[cache_key]
 
   def add_dp_noise(self, p):
     """Adding noise via flipping each bit with probability p."""
