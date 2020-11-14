15e228f59f2f633ed9bd1939dbc3e95b6819db08
huangxichen1 <64606379+huangxichen1@users.noreply.github.com>
2020-11-14 01:39:17 +0000

    Add meta voc configurations for Exp ADBF (#99)
    * Meta VoC.
    
    * Meta VoC eval config.
    
    * Meta VoC config.
    
    * Change meta voc config to local DP theme.
    
    * Add config for Meta VoC estimator for Uniform Bloom Filters. (#100)

--- src/evaluations/data/tests/evaluation_configs_test.py
+++ src/evaluations/data/tests/evaluation_configs_test.py
@@ -419,6 +419,38 @@ class EvaluationConfigTest(parameterized.TestCase):
     for x, y in zip(estimated, expected):
       self.assertAlmostEqual(x, y)
 
+  def test_meta_voc_for_exp_adbf(self):
+    conf = evaluation_configs._meta_voc_for_exp_adbf(
+        adbf_length=16,
+        adbf_decay_rate=2,
+        voc_length=4,
+        sketch_epsilon=1)
+    self.assertEqual(
+        conf.name,
+        'exp_bloom_filter-16_2-meta_voc_4-local_dp_1.0000-no_global_dp',
+        'Config name is not correct.')
+    exp_adbf = conf.sketch_factory(1)
+    self.assertLen(exp_adbf.sketch, 16)
+    self.assertEqual(conf.estimator.adbf_estimator._method, 'exp')
+    voc_sketch = conf.estimator.meta_sketch_factory(1)
+    self.assertLen(voc_sketch.stats, 4)
+
+  def test_meta_voc_for_bf(self):
+    conf = evaluation_configs._meta_voc_for_bf(
+        bf_length=16,
+        voc_length=4,
+        sketch_epsilon=1)
+    self.assertEqual(
+        conf.name,
+        'bloom_filter-16-meta_voc_4-local_dp_1.0000-no_global_dp',
+        'Config name is not correct.')
+    bf = conf.sketch_factory(1)
+    self.assertLen(bf.sketch, 16)
+    bf.add(1)
+    self.assertAlmostEqual(conf.estimator([bf])[0], 1.03, delta=1.03 * 0.1)
+    voc_sketch = conf.estimator.meta_sketch_factory(1)
+    self.assertLen(voc_sketch.stats, 4)
+
   def test_get_estimator_configs_return_configs(self):
     expected_sketch_estimator_configs = [conf.name for conf in (
         evaluation_configs._generate_cardinality_estimator_configs())]
