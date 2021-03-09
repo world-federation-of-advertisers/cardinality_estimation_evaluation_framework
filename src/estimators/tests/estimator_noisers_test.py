b236f684091b59dd3fba85e9a405cbd27a6a875d
Pasin Manurangsi <pasin@google.com>
2021-03-09 15:11:08 +0700

    Add Discrete Gaussian Noiser

--- src/estimators/tests/estimator_noisers_test.py
+++ src/estimators/tests/estimator_noisers_test.py
@@ -26,6 +26,7 @@ class FakeGeometricRandomState:
     self.calls_count += 1
     return self.return_values[self.calls_count - 1]
 
+
 class FakeGaussianRandomState:
 
   def __init__(self, return_value):
@@ -33,6 +34,23 @@ class FakeGaussianRandomState:
 
   def normal(self, size, scale):
     return self.return_value
+
+
+class FakeDiscreteGaussianRandomState:
+
+  def __init__(self, return_values_geometric, return_values_binomial):
+    self.return_values_geometric = return_values_geometric
+    self.return_values_binomial = return_values_binomial
+    self.calls_count_geometric = 0
+    self.calls_count_binomial = 0
+
+  def geometric(self, p):
+    self.calls_count_geometric += 1
+    return self.return_values_geometric[self.calls_count_geometric - 1]
+
+  def binomial(self, n, p):
+    self.calls_count_binomial += 1
+    return self.return_values_binomial[self.calls_count_binomial - 1]
 
 
 class EstimatorNoisersTest(absltest.TestCase):
@@ -72,6 +90,19 @@ class EstimatorNoisersTest(absltest.TestCase):
         1., .1, random_state=FakeGaussianRandomState([0.5, -0.5]))
     result = le(np.array([10., 20.]))
     np.testing.assert_array_equal(result, [10.5, 19.5])
+
+  def test_discrete_gaussian_estimate_noiser_accepts_scalar_argument(self):
+    le = estimator_noisers.DiscreteGaussianEstimateNoiser(
+        1., .1, random_state=FakeDiscreteGaussianRandomState([1.5, 1.], [1]))
+    result = le(10.)
+    self.assertEqual(result, 10.5)
+
+  def test_discrete_gaussian_estimate_noiser_accepts_array_argument(self):
+    le = estimator_noisers.DiscreteGaussianEstimateNoiser(
+        1., .1, random_state=FakeDiscreteGaussianRandomState(
+          [1.5, 1., -10., -30., 15., 13.7], [1, 0, 1]))
+    result = le(np.array([10., 20.]))
+    np.testing.assert_array_equal(result, [10.5, 21.3])
 
 
 if __name__ == '__main__':
